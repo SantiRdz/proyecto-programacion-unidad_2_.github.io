1f329ed592be370a06c657e67d470b12f2d45af6
SantiRdz <santiagoxguapo81@gmail.com>
2024-06-12 18:33:24 -0600

    Add files via upload

---
+++ proyecto programacion unidad_2/do_while.js
@@ -0,0 +1,74 @@
+function MyFuction() {
+
+    console.log(" EJ 1")
+    let i=0;
+
+    do
+    {
+        console.log(i)
+        i++;  
+    }
+        while (i<=5);
+ 
+
+    console.log("  EJ2")
+    let l=1;
+
+    do 
+    {
+         console.log(l)
+         l++;
+    }
+      while  (l<=3);
+
+     console.log(" EJ3")
+     let n=5;
+     
+     do 
+     {
+        console.log(n)
+        n++;
+     }
+     while (n<=20);
+
+     console.log(" EJ4")
+     let a=5;
+
+     do
+     {
+        console.log(a)
+        a=a+5;
+     }
+     while (a<100);
+
+     console.log(" EJ5")
+     let j=1
+
+     do
+     {
+        console.log(j);
+        j=j+100;
+     }
+     while (j<1000);
+
+     console.log(" EJ6")
+     let k=50;
+
+     do
+     {
+        console.log(k)
+        k++;
+     }
+     while (k>100);
+
+     console.log(" EJ7")
+     let q=10;
+
+     do
+     {
+        console.log(q)
+        q--;
+     }
+     while (q>0);
+ 
+}
